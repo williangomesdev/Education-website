7c1e565d93d2ea76988e201312e16ecc05740a99
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-02-21 16:16:34 -0300

    review styled complete and slide

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,4 @@
+//slide com marcadores abaixo
 var swiper = new Swiper(".home-slider", {
   pagination: {
     el: ".swiper-pagination",
@@ -7,6 +8,7 @@ var swiper = new Swiper(".home-slider", {
   grabCursor: true,
 });
 
+//slide sem marcadores 
 var swiper = new Swiper(".home-courses-slider", {
   loop: true,
   grabCursor: true,
@@ -24,6 +26,22 @@ var swiper = new Swiper(".home-courses-slider", {
 });
 
 var swiper = new Swiper(".teachers-slider", {
+  loop: true,
+  grabCursor: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
   loop: true,
   grabCursor: true,
   breakpoints: {
