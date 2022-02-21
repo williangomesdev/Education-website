346e9aa8e9dd9bf59b1106c63ca722b6d5969afa
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-02-21 17:38:52 -0300

    course page completed

---
+++ js/buttons.js
@@ -0,0 +1,8 @@
+let morecoursebtn = document.querySelector(".load-more .btn");
+
+morecoursebtn.onclick = () => {
+  document.querySelectorAll(".courses .box-container .hide").forEach((show) => {
+    show.style.display = "block";
+  });
+  morecoursebtn.style.display = "none";
+};
